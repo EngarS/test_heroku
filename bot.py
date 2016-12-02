f894eb09b2c8560d741b5c136ea7e936802d2d47
EngarS <satanist616>
2016-12-03 00:42:50 +0700

    Webhook test

--- bot.py
+++ bot.py
@@ -4,15 +4,15 @@ from flask import Flask, request
 
 bot = telebot.TeleBot(config.token)
 
-#server = Flask(__name__)
+server = Flask(__name__)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, "Ты написал: " + message.text)
 
-bot.polling(none_stop=True, interval=0, timeout=3)
+#bot.polling(none_stop=True, interval=0, timeout=3)
 
-'''
+
 @server.route('/' + config.token, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
@@ -25,5 +25,5 @@ def webhook():
     return "CONNECTED", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-'''
+
 
